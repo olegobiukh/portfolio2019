6cf4bb8709e9eba762dcc55c8e96d27110616e56
Oleg <oleg.obiukh@gmail.com>
2019-10-03 09:22:18 +0300

    Updates

---
+++ precache-manifest.c12601691059513c3db47f129ca03329.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2853582a008c1818773683cf527e453",
+    "url": "/portfolio2019/index.html"
+  },
+  {
+    "revision": "76a59e60a5f175d80aa1",
+    "url": "/portfolio2019/static/css/main.6e3c6ac0.chunk.css"
+  },
+  {
+    "revision": "6927969c7f9c4a23a675",
+    "url": "/portfolio2019/static/js/2.2d5b1274.chunk.js"
+  },
+  {
+    "revision": "76a59e60a5f175d80aa1",
+    "url": "/portfolio2019/static/js/main.dffe3228.chunk.js"
+  },
+  {
+    "revision": "adbe7e1532f47b48ffda",
+    "url": "/portfolio2019/static/js/runtime-main.a701de4d.js"
+  }
+]);
